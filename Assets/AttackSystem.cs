49561e3cdaddce9b23fb27bfc000c93307d65664
BorisTheLizard <99308714+BorisTheLizard@users.noreply.github.com>
2023-05-20 21:18:09 -0700

    attackSystem

---
+++ Assets/AttackSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSystem : MonoBehaviour
+{
+	[SerializeField] Animator anim;
+	float shootingSpeed;
+	[SerializeField] float maxShootingSpeed = 0.5f;
+	[SerializeField] GameObject hitCollider;
+	GameObjectPool pool;
+	[SerializeField] int bullets;
+	[SerializeField] Transform shootingPoint;
+
+	private void Start()
+	{
+		pool = FindObjectOfType<GameObjectPool>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			shooting();
+		}
+		if (Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			//Baton attack
+		}
+	}
+	private void shooting()
+	{
+		if (Time.time > shootingSpeed)
+		{
+			shootingSpeed = Time.time + maxShootingSpeed;
+			GameObject obj = pool.GetObject(bullets);
+			if (obj != null)
+			{
+				obj.SetActive(false);
+				obj.transform.position = shootingPoint.position;
+				obj.transform.rotation = shootingPoint.rotation;
+				obj.SetActive(true);
+			}
+		}
+
+	}
+}
